f80d05be909b9e0de06119c4db006b67c9567ff9
Michael Giordano <mgiord1987@gmail.com>
2022-12-10 17:37:19 -0500

    compliance with pylint

--- day_3/day_3.py
+++ day_3/day_3.py
@@ -1,3 +1,6 @@
+"""
+Day Three
+"""
 import string
 
 INPUT = 'input.txt'
@@ -8,16 +11,32 @@ ITEM_PRIORITIES = dict(zip(string.ascii_lowercase + string.ascii_uppercase,
 
 
 def get_compartments(rucksack):
+    """
+    Sorts the letters in rucksack evenly into compartments
+    :param rucksack: string of random letters from input file
+    :return: tuple of lists, each taking half of the letters in rucksack
+    """
     rucksack = rucksack.replace('\n', '')
     halfway = int(len(rucksack) / 2)
-    c1 = rucksack[:halfway]
-    c2 = rucksack[halfway:]
-    return (c1, c2)
+    left_compartment = rucksack[:halfway]
+    right_compartment = rucksack[halfway:]
+    return (left_compartment, right_compartment)
+
 
 def find_duplicate_item(rucksack):
+    """
+    Returns the single item in each rucksack that is erroneously in both
+    compartments
+    :param rucksack: tuple of lists
+    :return: string of the letter that is in both lists in tuple rucksack
+    """
     return [item for item in rucksack[0] if item in rucksack[1]][0]
 
+
 def main():
+    """
+    runs through input.txt and prints answers
+    """
     total_score = 0
     with open(INPUT, 'r', encoding="utf-8") as rucksacks:
         for rucksack in rucksacks.readlines():
