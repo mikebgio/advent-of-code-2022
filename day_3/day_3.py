696563d12bb74ada8cc17765c640895c27747156
Michael Giordano <mgiord1987@gmail.com>
2022-12-11 14:21:35 -0500

    completes part two of day three

--- day_3/day_3.py
+++ day_3/day_3.py
@@ -33,18 +33,74 @@ def find_duplicate_item(rucksack):
     return [item for item in rucksack[0] if item in rucksack[1]][0]
 
 
+def get_part_one_answer(rucksacks):
+    """
+    iterates over list of rucksacks and returns the answer for part one
+    :param rucksacks: list[str]
+    :return: int
+    """
+    total_score = 0
+    for rucksack in rucksacks:
+        sorted_rucksack = get_compartments(rucksack)
+        duplicate_item = find_duplicate_item(sorted_rucksack)
+        priority_score = ITEM_PRIORITIES[duplicate_item]
+        total_score += priority_score
+    return total_score
+
+
+def get_badge_letter_from_rucksack_group(rucksack_group):
+    """
+    Finds the letter that is repeated in a list of three strings
+    :param rucksack_group: list[str]
+    :return: str
+    """
+    for letter in rucksack_group[0]:
+        if all(letter in rucksack for rucksack in rucksack_group):
+            return letter
+    return None
+
+
+def process_rucksack_groups(rucksacks):
+    """
+    Finds which letter appears in each of the rucksack strings in a rucksack
+    group, returning that letter
+    :param rucksack_group: list[str]
+    :return: str
+    """
+    rucksack_group = []
+    full_list = []
+    for idx, rucksack in enumerate(rucksacks):
+        rucksack_group.append(rucksack)
+        if (idx + 1) % 3 == 0:
+            full_list.append(rucksack_group)
+            rucksack_group = []
+    return full_list
+
+
+def get_part_two_answer(rucksacks):
+    """
+    Processes the input file for part two challenge of day 3
+    :param rucksacks: list[str]
+    :return: int
+    """
+    rucksack_groups = process_rucksack_groups(rucksacks)
+    priority_points = 0
+    for rucksack_group in rucksack_groups:
+        badge = get_badge_letter_from_rucksack_group(rucksack_group)
+        priority_points += ITEM_PRIORITIES[badge]
+    return priority_points
+
+
 def main():
     """
     runs through input.txt and prints answers
     """
-    total_score = 0
-    with open(INPUT, 'r', encoding="utf-8") as rucksacks:
-        for rucksack in rucksacks.readlines():
-            sorted_rucksack = get_compartments(rucksack)
-            duplicate_item = find_duplicate_item(sorted_rucksack)
-            priority_score = ITEM_PRIORITIES[duplicate_item]
-            total_score += priority_score
-    print(f'Total priority score is: {total_score}')
+    with open(INPUT, 'r', encoding="utf-8") as rucksacks_file:
+        rucksacks = rucksacks_file.readlines()
+    part_one_answer = get_part_one_answer(rucksacks)
+    part_two_answer = get_part_two_answer(rucksacks)
+    print(f'Total priority score is: {part_one_answer}')
+    print(f'Total sum of badge priorities: {part_two_answer}')
 
 
 if __name__ == '__main__':
